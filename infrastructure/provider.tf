a071adb553511bc335f9b91d5c624c4021a711ff
EthemKesim <kesimethem@gmail.com>
2026-09-01 18:19:44 +0300

    Enable Terraform plan with AWS OIDC

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+  bucket       = "cloudtask-terraform-state-ethem"
+  key          = "cloudtask/terraform.tfstate"
+  region       = "us-east-1"
+  encrypt      = true
+  use_lockfile = true
+}
   required_providers {
     aws = {
       source  = "hashicorp/aws"
