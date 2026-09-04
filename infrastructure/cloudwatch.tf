a876d8c95cb9517c69f5c9f2560e2a72834340c4
EthemKesim <kesimethem@gmail.com>
2026-09-04 15:30:45 +0300

    Add CloudWatch monitoring and SNS alerts

--- infrastructure/cloudwatch.tf
+++ infrastructure/cloudwatch.tf
@@ -1,3 +1,8 @@
+resource "aws_cloudwatch_log_group" "lambda" {
+  name              = "/aws/lambda/cloudtask-api"
+  retention_in_days = 14
+}
+
 resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
   alarm_name        = "cloudtask-lambda-errors"
   alarm_description = "Triggers when CloudTask Lambda reports errors."
@@ -6,9 +11,10 @@ resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
   metric_name = "Errors"
   statistic   = "Sum"
 
-  period              = 300
-  evaluation_periods  = 1
-  threshold           = 1
+  period             = 300
+  evaluation_periods = 1
+  threshold          = 1
+
   comparison_operator = "GreaterThanOrEqualToThreshold"
 
   dimensions = {
@@ -16,4 +22,22 @@ resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
   }
 
   treat_missing_data = "notBreaching"
+
+  alarm_actions = [
+    aws_sns_topic.cloudtask_alerts.arn
+  ]
+
+  ok_actions = [
+    aws_sns_topic.cloudtask_alerts.arn
+  ]
+}
+
+resource "aws_sns_topic" "cloudtask_alerts" {
+  name = "cloudtask-alerts"
+}
+
+resource "aws_sns_topic_subscription" "email" {
+  topic_arn = aws_sns_topic.cloudtask_alerts.arn
+  protocol  = "email"
+  endpoint  = "kesimethem@gmail.com"
 }
